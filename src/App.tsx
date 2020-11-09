71b8b77126ef0e5686818b0858d83e64015f4936
Vladislav Olhovik <alkhovikvlad@gmail.com>
2020-11-09 18:09:08 +0100

    redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,53 @@
-import React, { useState } from 'react';
-import './App.css';
-import Counter from './components/Counter';
-import Tuner from './components/Tuner';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import Counter from "./components/Counter";
+import Tuner from "./components/Tuner";
+import { RootStateType } from "./redux/store";
+import {
+  StateType,
+  addMaxValueAC,
+  addMinValueAC,
+  counterAC,
+  saveValueAC,
+} from "./redux/app-reducer";
 
 function App() {
-  type StateType={
-    maxValue:number,
-    startValue:number,
-    value:number|string,
-    error:boolean,
-  }
-  let [state, setState] = useState<StateType>({
-    maxValue:5,
-    value:"enter values and press 'set'",
-    startValue:0,
-    error:false,
-  })
-  //@ts-ignore
-  window.state=state
-  const addMaxValue=(value:number)=>{
-    if(value<=state.startValue||state.startValue<0){
-      setState({...state,value:"Incorrect value!",maxValue: value, error: true})
-    }else
-    setState({...state,value:"enter values and press 'set'",maxValue: value, error: false})
-  }
-  const addMinValue=(value:number)=>{
-    if(value<0||value>=state.maxValue){
-      setState({...state,value:"Incorrect value!",startValue: value, error: true})
-    }else
-    setState({...state,value:"enter values and press 'set'",startValue: value, error: false})
-  }
- 
-  const saveValue=()=>{
-    setState({...state,value:state.startValue})
-  }
+  const state = useSelector<RootStateType, StateType>((state) => state.app);
+  const dispatch = useDispatch();
+
+  const addMaxValue = (value: number) => {
+    dispatch(addMaxValueAC(value));
+  };
+  const addMinValue = (value: number) => {
+    dispatch(addMinValueAC(value));
+  };
+
+  const saveValue = () => {
+    dispatch(saveValueAC());
+  };
   const counter = () => {
-    if(typeof state.value === 'number')
-    state.value !== state.maxValue && setState({...state,value:++state.value})
-  }
-  
+    dispatch(counterAC());
+  };
 
   return (
-    <div className='wrapper'>
-      <Tuner maxValue={state.maxValue}
-             value={state.value}
-             minValue={state.startValue}
-             saveValue={saveValue}
-             addMaxValue={addMaxValue}
-             addMinValue={addMinValue}
-             error = {state.error}/>
-      <Counter maxValue={state.maxValue} error = {state.error} counter={counter} reset={saveValue} startValue={state.value}/>
+    <div className="wrapper">
+      <Tuner
+        maxValue={state.maxValue}
+        value={state.value}
+        minValue={state.startValue}
+        saveValue={saveValue}
+        addMaxValue={addMaxValue}
+        addMinValue={addMinValue}
+        error={state.error}
+      />
+      <Counter
+        maxValue={state.maxValue}
+        error={state.error}
+        counter={counter}
+        reset={saveValue}
+        startValue={state.value}
+      />
     </div>
   );
 }
---
+++ src/redux/app-reducer.ts
@@ -0,0 +1,43 @@
+ export type StateType={
+    maxValue:number,
+    startValue:number,
+    value:number|string,
+    error:boolean,
+}
+type ActionAppReducerType = ReturnType<typeof addMaxValueAC>
+|ReturnType<typeof addMinValueAC>
+|ReturnType<typeof saveValueAC>
+|ReturnType<typeof counterAC>
+let initState:StateType = {
+    maxValue:5,
+    value:"enter values and press 'set'",
+    startValue:0,
+    error:false,
+}
+
+export const appReducer = (state = initState, action: ActionAppReducerType) => {
+    switch (action.type) {
+        case 'ADD_MAX_VALUE':
+            return (action.value <= state.startValue || state.startValue < 0)
+                ? { ...state, value: "Incorrect value!", maxValue: action.value, error: true }
+                : { ...state, value: "enter values and press 'set'", maxValue: action.value, error: false }
+        case 'ADD_MIN_VALUE':
+            return (action.value < 0 || action.value >= state.maxValue)
+                ? { ...state, value: "Incorrect value!", startValue: action.value, error: true }
+                : { ...state, value: "enter values and press 'set'", startValue: action.value, error: false }
+        case 'SAVE_VALUE':
+            return { ...state, value: state.startValue }
+        case 'NEW_COUNT':
+            return typeof state.value === 'number'
+                ? { ...state, value: ++state.value }
+                : state
+        default: return state
+    }
+}
+
+
+
+export const addMaxValueAC = (value:number) => ({type:'ADD_MAX_VALUE', value} as const) 
+export const addMinValueAC = (value:number) => ({type:'ADD_MIN_VALUE', value} as const) 
+export const saveValueAC = () => ({type:'SAVE_VALUE'} as const) 
+export const counterAC = () => ({type:'NEW_COUNT'} as const) 
