a1d29ff7fc5eb9cd7775a7c29cb7bd790e30fc04
Vladislav Olhovik <alkhovikvlad@gmail.com>
2020-11-09 19:00:13 +0100

    Updates

---
+++ precache-manifest.c0a8b021454f8f06c1351388762e7be5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c14248eb7b4a989cd33f0a65ee991c28",
+    "url": "/My-counterTS/index.html"
+  },
+  {
+    "revision": "f9de11f9f6fa6563a161",
+    "url": "/My-counterTS/static/css/main.7b34a647.chunk.css"
+  },
+  {
+    "revision": "324f22d825537ccbe884",
+    "url": "/My-counterTS/static/js/2.a0bde023.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-counterTS/static/js/2.a0bde023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9de11f9f6fa6563a161",
+    "url": "/My-counterTS/static/js/main.ce2dece8.chunk.js"
+  },
+  {
+    "revision": "0e2dd2fbd0dc9499f915",
+    "url": "/My-counterTS/static/js/runtime-main.63ad7202.js"
+  }
+]);
